792684eabcc8b4daf4a141bb15b6f094c813682d
Abhinav Singh <official.abhinavsingh15@gmail.com>
2026-03-30 22:21:29 +0530

    worked on workflow page

---
+++ src/features/editor/components/editor-header.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  useSuspenseWorkflow,
+  useUpdateWorkflowName,
+} from "@/features/workflows/hooks/use-workflows";
+import { SaveIcon } from "lucide-react";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+
+export const EditorSaveButton = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <div className="ml-auto">
+      <Button size="sm" onClick={() => {}} disabled={false}>
+        <SaveIcon className="size-4" />
+        Save
+      </Button>
+    </div>
+  );
+};
+
+export const EditorNameInput = ({ workflowId }: { workflowId: string }) => {
+  const { data: workflow } = useSuspenseWorkflow(workflowId);
+  const updateWorkflow = useUpdateWorkflowName();
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(workflow?.name || "");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (workflow.name) {
+      setName(workflow.name);
+    }
+  }, [workflow.name]);
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    }
+  }, [isEditing]);
+
+  const handleSave = async () => {
+    if (name.trim() === "" || name === workflow?.name) {
+      setIsEditing(false);
+      return;
+    }
+    setIsEditing(false);
+
+    try {
+      await updateWorkflow.mutateAsync({ id: workflowId, name });
+    } catch (error) {
+      setName(workflow?.name || "");
+      console.error("Error updating workflow name:", error);
+    } finally {
+      setIsEditing(false);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleSave();
+    } else if (e.key === "Escape") {
+      e.preventDefault();
+      setName(workflow?.name || "");
+      setIsEditing(false);
+    }
+  };
+
+  if (isEditing) {
+    return (
+      <Input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        onBlur={handleSave}
+        onKeyDown={handleKeyDown}
+        disabled={updateWorkflow.isPending}
+        className="w-auto h-7 text-sm px-2"
+      />
+    );
+  }
+
+  return (
+    <BreadcrumbItem
+      className="cursor-pointer hover:text-foreground transition-colors"
+      onClick={() => setIsEditing(true)}
+    >
+      {workflow?.name}
+    </BreadcrumbItem>
+  );
+};
+
+export const EditorBreadcrumbs = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link prefetch href="/workflows">
+              Workflows
+            </Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <EditorNameInput workflowId={workflowId} />
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export const EditorHeader = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <header className="h-14 flex items-center gap-2 px-4 border-b bg-background">
+      <SidebarTrigger />
+      <div className="flex flex-row items-center justify-between gap-x-4 w-full">
+        <EditorBreadcrumbs workflowId={workflowId} />
+        <EditorSaveButton workflowId={workflowId} />
+      </div>
+    </header>
+  );
+};
--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -52,3 +52,32 @@ export const useRemoveWorkflow = () => {
     }),
   );
 };
+
+// Hook to single workflow using suspense
+export const useSuspenseWorkflow = (id: string) => {
+  const trpc = useTRPC();
+
+  return useSuspenseQuery(trpc.workflows.getOne.queryOptions({ id }));
+};
+
+export const useUpdateWorkflowName = () => {
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+
+  return useMutation(
+    trpc.workflows.updateName.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Workflow ${data.name} updated`);
+        queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions({}));
+        queryClient.invalidateQueries(
+          trpc.workflows.getOne.queryOptions({
+            id: data.id,
+          }),
+        );
+      },
+      onError: (error) => {
+        toast.error(`Failer to update the workflow: ${error.message}`);
+      },
+    }),
+  );
+};
